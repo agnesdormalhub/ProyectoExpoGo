3d0be43a2bf4bca635ab077232d9aa0c0ac10c2a
Delfivillanueva <villandelfi@gmail.com>
2021-10-25 15:53:47 -0300

    IsLoggedIn y Post

--- src/Components/AuthDrawer.js
+++ src/Components/AuthDrawer.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react'
 import { NavigationContainer } from '@react-navigation/native'
 import { createDrawerNavigator } from '@react-navigation/drawer'
+import Home from '../Screens/Home'
 
 import Register from '../Screens/Register'
 import Login from '../Screens/Login'
@@ -11,6 +12,10 @@ const Drawer = createDrawerNavigator();
 export default class AuthDrawer extends Component{
     constructor(props){
         super(props);
+        this.state= {
+            isLoggedIn: false,
+            error: ''
+        }
 
     }
 
@@ -18,12 +23,12 @@ export default class AuthDrawer extends Component{
     registrarse(email,password){
         auth.createUserWithEmailAndPassword(email,password)
         .then(response => {
-            this.setState({loggedIn: true})
+            this.setState({isLoggedIn: true})
         })
 
     .catch(error => {
         console.log(error);
-        this.setState({loggedIn:false})
+        this.setState({isLoggedIn:false, error: 'Error en el registro'})
     })
     }
 
@@ -31,20 +36,41 @@ export default class AuthDrawer extends Component{
     ingresar (email, password){
         auth.signInWithEmailAndPassword(email,password)  
         .then(response => {
-            this.setState({loggedIn: true})
+            this.setState({isLoggedIn: true})
         })
 
     .catch(error => {
         console.log(error);
-        this.setState({loggedIn:false})
+        this.setState({isLoggedIn:false, error: 'Error en el loggeo'})
     })
       }
     render(){
         return(
             <NavigationContainer>
                 <Drawer.Navigator>
-                    <Drawer.Screen name="Register" component={()=> <Register registrarse =  {(email, password) => this.registrarse(email,password)}/>} />
-                    <Drawer.Screen name="Login" component= {()=> <Login ingresar = {(email, password) => this.ingresar(email,password)}/>} />
+                    {
+                        this.state.isLoggedIn ?
+                        <React.Fragment>
+                            <Drawer.Screen name="Home">
+                                {() => <Home/>}
+                            </Drawer.Screen>
+                        <Drawer.Screen name= 'Profile'>
+                            {()=> <Home/>}
+                        </Drawer.Screen>
+
+                        </React.Fragment>
+                        :
+                        <React.Fragment>
+                            <Drawer.Screen name="Login">
+                                {()=> <Login ingresar = {(email, password) => this.ingresar(email,password)}/>} 
+                            </Drawer.Screen>
+                            <Drawer.Screen name="Register">
+                                 {()=> <Register registrarse =  {(email, password) => this.registrarse(email,password)}/>} 
+                            </Drawer.Screen>
+                        </React.Fragment>
+                    }
+                   
+                    
                 </Drawer.Navigator>
             </NavigationContainer>
         )
--- src/Screens/Post.js
+++ src/Screens/Post.js
@@ -1,40 +1,53 @@
 import React, {Component} from "react";
 import {View, Text, TextInput, TouchableOpacity} from 'react-native';
+import { db, auth } from "../Firebase/Config";
 import {styles} from '../Styles/Styles'
 
 class Post extends Component{
-    constructor(){
+    constructor(props){
         super();
         this.state = {
-            post : '',
-            userName: '',
-            password: ''
+            title : '',
+            description: ""
         }
     }
 
+    createPost(){
+        db.collection('posts').add({
+            username: auth.currentUser.displayName,
+            title: this.state.title,
+            description: this.state.description,
+            createdAt: Date.now()
+        })
+        .then(response => {
+            console.log(response)
+        })
+        .catch(error =>{
+            console.log(error)
+        })
+    }
 
     render(){
-        return(
+        return( 
+
             <View style= {styles.container}>
                 <Text style={styles.titulo}> Publicacion </Text>
                 <Text style={styles.textoIn}> Postear </Text>
                 <TextInput
                     style ={styles.input}
                     placeholder = 'Introducir publicacion'
-                    keyboardType = 'multiline'
-                    onChangeText = { (text) => this.setState({post: text})} 
+                    onChangeText = { (text) => this.setState({title: text})} 
                 />
                 <Text style={styles.textoIn}> Contraseña </Text>
                 <TextInput
                  style ={styles.input}
-                 placeholder = 'Introducir contraseña'
-                 secureTextEntry={true}
-                 onChangeText = { (text) => this.setState({password: text})} 
+                 placeholder = 'Introducir descripción'
+                 keyboardType= 'multiline'
+                 onChangeText = { (text) => this.setState({description: text})} 
              />
-
                 
-                <TouchableOpacity style = {styles.boton} onPress = {()=> this.props.registrarse(this.state.email,this.state.password)}>
-                    <Text style ={styles.botonText}>Enviar</Text>
+                <TouchableOpacity style = {styles.boton} onPress = {()=> this.createPost()}>
+                    <Text style ={styles.botonText}>Crear</Text>
                 </TouchableOpacity>
             </View>
         )
